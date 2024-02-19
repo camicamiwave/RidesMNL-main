20cebe7fcf1e6467a6d45997e46fe079d8d5d7b6
Frankenstein <gadildonitarose@gmail.com>
2024-02-19 23:28:37 +0800

    cleaned

---
+++ RidesMNL-main/includes/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>RidesMNL - Admin</title>
+        <!-- plugins:css -->
+        <link rel="stylesheet" href="../vendors/feather/feather.css">
+        <link rel="stylesheet" href="../vendors/ti-icons/css/themify-icons.css">
+        <link rel="stylesheet" href="../vendors/css/vendor.bundle.base.css">
+        <!-- endinject -->
+        <!-- Plugin css for this page -->
+        <link rel="stylesheet" href="../vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+        <link rel="stylesheet" href="../vendors/ti-icons/css/themify-icons.css">
+        <link rel="stylesheet" type="text/css" href="../js/select.dataTables.min.css">
+        <!-- End plugin css for this page -->
+        <!-- inject:css -->
+        <link rel="stylesheet" href="../css/vertical-layout-light/style.css">
+        <!-- endinject -->
+        <link rel="shortcut icon" href="../images/favicon.png" />
+    </head>
+
+    <body>
+        <div class="container-scroller">
+            <?php include('navbar.php'); ?>
+            <div class="container-fluid page-body-wrapper">
+                <?php include('sidebar.php'); ?>
+                <!-- partial -->
+                <div class="main-panel">
+                    <div class="content-wrapper">
+                    
+                    </div>
+                    <!-- content-wrapper ends -->
+                    <?php include('footer.php'); ?>
