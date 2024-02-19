e8006f0c69689390338fe96b126e9633406fad07
Frankenstein <gadildonitarose@gmail.com>
2024-02-20 00:07:26 +0800

    update

--- RidesMNL-main/includes/header.php
+++ RidesMNL-main/includes/header.php
@@ -7,19 +7,19 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>RidesMNL - Admin</title>
         <!-- plugins:css -->
-        <link rel="stylesheet" href="../vendors/feather/feather.css">
-        <link rel="stylesheet" href="../vendors/ti-icons/css/themify-icons.css">
-        <link rel="stylesheet" href="../vendors/css/vendor.bundle.base.css">
+        <link rel="stylesheet" href="vendors/feather/feather.css">
+        <link rel="stylesheet" href="vendors/ti-icons/css/themify-icons.css">
+        <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
         <!-- endinject -->
         <!-- Plugin css for this page -->
-        <link rel="stylesheet" href="../vendors/datatables.net-bs4/dataTables.bootstrap4.css">
-        <link rel="stylesheet" href="../vendors/ti-icons/css/themify-icons.css">
-        <link rel="stylesheet" type="text/css" href="../js/select.dataTables.min.css">
+        <link rel="stylesheet" href="vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+        <link rel="stylesheet" href="vendors/ti-icons/css/themify-icons.css">
+        <link rel="stylesheet" type="text/css" href="js/select.dataTables.min.css">
         <!-- End plugin css for this page -->
         <!-- inject:css -->
-        <link rel="stylesheet" href="../css/vertical-layout-light/style.css">
+        <link rel="stylesheet" href="css/vertical-layout-light/style.css">
         <!-- endinject -->
-        <link rel="shortcut icon" href="../images/favicon.png" />
+        <link rel="shortcut icon" href="images/favicon.png" />
     </head>
 
     <body>
@@ -29,8 +29,4 @@
                 <?php include('sidebar.php'); ?>
                 <!-- partial -->
                 <div class="main-panel">
-                    <div class="content-wrapper">
                     
-                    </div>
-                    <!-- content-wrapper ends -->
-                    <?php include('footer.php'); ?>
