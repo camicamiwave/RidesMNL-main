94fb0855fbe01ee0466f480079aed6cd65ae653f
Frankenstein <gadildonitarose@gmail.com>
2024-02-20 01:36:51 +0800

    links and pages

--- RidesMNL-main/includes/sidebar.php
+++ RidesMNL-main/includes/sidebar.php
@@ -2,38 +2,38 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
   <ul class="nav">
     <li class="nav-item">
-      <a class="nav-link" href="index.html">
+      <a class="nav-link" href="index.php">
         <i class="icon-grid menu-icon"></i>
         <span class="menu-title">Dashboard</span>
       </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="index.html">
-        <i class="icon-grid menu-icon"></i>
+      <a class="nav-link" href="feedback.php">
+        <i class="icon-speech-bubble menu-icon"></i>
         <span class="menu-title">User Feedback</span>
       </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="index.html">
-        <i class="icon-grid menu-icon"></i>
+      <a class="nav-link" href="attraction.php">
+        <i class="icon-map menu-icon"></i>
         <span class="menu-title">Attraction List</span>
       </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="index.html">
+      <a class="nav-link" href="rates.php">
         <i class="icon-grid menu-icon"></i>
         <span class="menu-title">Transportation Rates</span>
       </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="index.html">
-        <i class="icon-grid menu-icon"></i>
+      <a class="nav-link" href="contact.php">
+        <i class="icon-head menu-icon"></i>
         <span class="menu-title">Contact Support</span>
       </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="index.html">
-        <i class="icon-grid menu-icon"></i>
+      <a class="nav-link" href="settings.php">
+        <i class="icon-help menu-icon"></i>
         <span class="menu-title">Settings</span>
       </a>
     </li>
