0f9caec5a982b98785d74159a96c438b0f1ec104
Frankenstein <gadildonitarose@gmail.com>
2024-02-20 16:09:16 +0800

    login

--- RidesMNL-main/rates.php
+++ RidesMNL-main/rates.php
@@ -1,7 +1,15 @@
 <?php include('includes/header.php'); ?>
 
     <div class="content-wrapper">
-                    
+        <div class="row">
+            <div class="col-md-12 grid-margin">
+              <div class="row">
+                <div class="col-12 col-xl-8 mb-4 mb-xl-0">
+                  <h3 class="font-weight-bold">Transportation Rates</h3>
+                </div>
+              </div>
+            </div>
+        </div>            
     </div>
     <!-- content-wrapper ends -->
 <?php include('includes/footer.php'); ?>
