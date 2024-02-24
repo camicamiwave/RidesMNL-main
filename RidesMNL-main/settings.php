0078d67dab93d91518812fe99873a4e9520ecf41
Frankenstein <gadildonitarose@gmail.com>
2024-02-24 21:42:31 +0800

    settings_additional

--- RidesMNL-main/settings.php
+++ RidesMNL-main/settings.php
@@ -8,44 +8,26 @@
               <h3 class="font-weight-bold">
                 <a href="javascript:history.go(-1);" class="btn btn-link">
                 <i class="icon-arrow-left menu-icon"></i></a>
-                API Key of App</h3>
-            </div>
-            <div class="col-12 col-xl-4">
-              <div class="justify-content-end d-flex">
-                <div class="dropdown flex-md-grow-1 flex-xl-grow-0">
-                  <button type="button" class="btn btn-success btn-icon-text">
-                    <i class="fa fa-plus" aria-hidden="true"></i>                                                   
-                    Add
-                  </button>
-                  <button type="button" class="btn btn-info btn-icon-text">
-                    <i class="fa fa-pencil" aria-hidden="true"></i> 
-                    Edit                                                                             
-                  </button>
-                  <button type="button" class="btn btn-danger btn-icon-text">
-                    <i class="fa fa-trash" aria-hidden="true"></i>                        
-                    Delete
-                  </button>
-                </div>
-              </div>
+               Settings</h3>
             </div>
           </div>
         </div>
     </div> 
 
-    <div class="row" style="align-items: center;">
-      <div class="col-xl-12 grid-margin stretch-card">
-        <div class="card">
-          <div class="card-body">
-            <div class="form-group row">
-              <div class="col">
-                <label>API Key of App</label>
-                <div id="the-basics">
-                  <input class="typeahead" type="text" value="123456789" readonly>
-                </div>
-              </div>
-              <div class="col-sm-2 mt-4" style="text-align: center;"></div>
-              <div class="col"></div>
+    <!-- Cards-->
+    <a href="settings_api.php" style="text-decoration: none;">
+        <div class="card mb-3" style="max-width: 350px;">
+            <div class="row no-gutters">
+            <div class="col-md-4 m-auto pl-3">
+                <i class="fa fa-map" aria-hidden="true" style="font-size:30px; color: #2900ff; padding:10%; border-radius:100%;"></i>
             </div>
-  </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                <h5 class="card-title m-auto" style="color:#2900ff;">API Key of App</h5>
+                </div>
+            </div>
+            </div>
+        </div>
+    </a>
   <!-- content-wrapper ends -->
 <?php include('includes/footer.php'); ?>
