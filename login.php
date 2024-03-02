33f3c9ec44eda5ad0b36bae90d5810b8ea470007
camicamiwave <camilabinas4@gmail.com>
2024-03-02 10:29:46 +0800

    .

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>RidesMNL -  Admin</title>
+  <title>RidesMNL - Admin</title>
   <!-- plugins:css -->
   <link rel="stylesheet" href="vendors/feather/feather.css">
   <link rel="stylesheet" href="vendors/ti-icons/css/themify-icons.css">
@@ -20,49 +20,94 @@
 </head>
 
 <body>
-    <div class="container-scroller">
-        <div class="container-fluid page-body-wrapper full-page-wrapper">
-            <div class="content-wrapper d-flex align-items-center auth px-0">
-                <div class="row w-100 mx-0">
-                    <div class="col-lg-4 mx-auto">
-                        <div class="auth-form-light text-left py-5 px-4 px-sm-5">
-                            <div class="brand-logo text-center img-fluid">
-                                <img src="images/logo_1.png" alt="logo">
-                            </div>
-                            <form class="pt-3">
-                                <h6>Username</h6>
-                                <div class="form-group">
-                                    <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Enter your username">
-                                </div>
-                                <h6>Password</h6>
-                                <div class="form-group">
-                                    <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Enter your password">
-                                </div>
-                                <div class="mt-3">
-                                    <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="index.php">Log In</a>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper d-flex align-items-center auth px-0">
+        <div class="row w-100 mx-0">
+          <div class="col-lg-4 mx-auto">
+            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+              <div class="brand-logo text-center img-fluid">
+                <img src="images/logo_1.png" alt="logo">
+              </div>
+              <form class="pt-3">
+
+                <h6>Username</h6>
+                <div class="form-group">
+                  <input type="text" class="form-control form-control-lg" id="exampleInputUsername1" placeholder="Enter your username">
                 </div>
+
+
+                <h6>Password</h6>
+                <div class="form-group">
+                  <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Enter your password">
+                </div>
+                <div class="mt-3">
+                  <button class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" id="login">Log In</button>
+                </div>
+              </form>
             </div>
-            <!-- content-wrapper ends -->
+          </div>
         </div>
-        <!-- page-body-wrapper ends -->
+      </div>
+      <!-- content-wrapper ends -->
     </div>
-    <!-- container-scroller -->
-    <!-- plugins:js -->
-    <script src="vendors/js/vendor.bundle.base.js"></script>
-    <!-- endinject -->
-    <!-- Plugin js for this page -->
-    <!-- End plugin js for this page -->
-    <!-- inject:js -->
-    <script src="js/off-canvas.js"></script>
-    <script src="js/hoverable-collapse.js"></script>
-    <script src="js/template.js"></script>
-    <script src="js/settings.js"></script>
-    <script src="js/todolist.js"></script>
-    <!-- endinject -->
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <script src="vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="js/off-canvas.js"></script>
+  <script src="js/hoverable-collapse.js"></script>
+  <script src="js/template.js"></script>
+  <script src="js/settings.js"></script>
+  <script src="js/todolist.js"></script>
+  <!-- endinject -->
+
+  <!-- Firebase SDK -->
+  <script src="https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"></script>
+
+  <script type="module">
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+    import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+
+    const firebaseConfig = {
+    apiKey: "AIzaSyCPSg2yG1TnhYgzK2aNEVpbxMf4w2qwYP0",
+    authDomain: "ridesmnl-48ff4.firebaseapp.com",
+    databaseURL: "https://ridesmnl-48ff4-default-rtdb.firebaseio.com",
+    projectId: "ridesmnl-48ff4",
+    storageBucket: "ridesmnl-48ff4.appspot.com",
+    messagingSenderId: "51707026546",
+    appId: "1:51707026546:web:96df015fa7a950149836eb",
+    measurementId: "G-N3C1209JN6"
+    };
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+    // get ref to database services
+    const db = getDatabase(app);
+
+    document.getElementById("login").addEventListener('click', function(e) {
+      e.preventDefault(); // prevent form submission
+
+      const username = document.getElementById("exampleInputUsername1").value;
+
+      // Write user data to the database
+      set(ref(db, 'accounts/' + username), {
+        username: username
+      });
+
+      alert("Login Successfully!");
+       
+    // Redirect to the dashboard page
+    window.location.href = "index.php";
+    });
+
+  </script>
 </body>
 
 </html>
---
+++ scr/attraction.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCuU5Ez73upjqInnKGiyNyz9AP6azKB7VQ",
+    authDomain: "ridesmnl-web.firebaseapp.com",
+    projectId: "ridesmnl-web",
+    storageBucket: "ridesmnl-web.appspot.com",
+    messagingSenderId: "1047345129505",
+    appId: "1:1047345129505:web:2a9289e36befcab20ddb60"
+  }
+
+initializeApp(firebaseConfig)
